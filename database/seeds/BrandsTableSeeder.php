6a62060a7e8f041343e9ce6a64475a909660dda6
ratnofirmansyah <asturof11@gmail.com>
2020-10-04 14:28:27 +0700

    add new seeder

---
+++ database/seeds/BrandsTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BrandsTableSeeder extends Seeder
+{
+    public function run()
+    {
+        
+
+        \DB::table('brands')->delete();
+        
+        \DB::table('brands')->insert(array (
+            0 => 
+            array (
+                'id' => 1,
+                'name' => 'Firman Sembako',
+                'avatar' => 'brands\\October2020\\ABDnq48lhZFoCDc794WL.jpg',
+                'url' => 'https://firmansembako.business.site/',
+                'created_at' => '2020-10-03 05:11:27',
+                'updated_at' => '2020-10-03 05:11:27',
+            ),
+        ));
+        
+        
+    }
+}
