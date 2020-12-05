4ea5c53278cb3b0a8325038528da5b3d8bf5d2da
ratnofirmansyah <asturof11@gmail.com>
2020-12-05 18:55:01 +0700

    add migration

---
+++ database/migrations/2020_12_05_115031_modify_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ModifyUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('brand_id')->after('role_id')->nullable()->default(null)->change();
+
+            $table->foreign('brand_id')
+                ->references('id')
+                ->on('brands');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
