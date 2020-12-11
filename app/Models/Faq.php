0dfafd67fb9ce64c67e26bbbf1fb063090763b99
ratnofirmansyah <asturof11@gmail.com>
2020-12-11 21:24:41 +0700

    add faq model

---
+++ app/Models/Faq.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Faq extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $table = 'faqs';
+    public $timestamps = true;
+
+    protected $fillable = [
+        'question',
+        'answer'
+    ];
+
+    protected $hidden = ['created_at', 'updated_at', 'deleted_at'];
+}
