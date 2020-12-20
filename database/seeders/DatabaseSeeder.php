9dde378c3ae850fae8b6f31e082e56e28d6a723a
ratnofirmansyah <asturof11@gmail.com>
2020-12-20 15:54:23 +0700

    update database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,13 +14,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // User::factory(10)->create();
-        $this->call(DataRowsTableSeeder::class);
-        $this->call(DataTypesTableSeeder::class);
-        $this->call(MenuItemsTableSeeder::class);
-        $this->call(RolesTableSeeder::class);
-        $this->call(PermissionRoleTableSeeder::class);
-        $this->call(PermissionsTableSeeder::class);
-        $this->call(SettingsTableSeeder::class);
+        // $this->call(DataRowsTableSeeder::class);
+        // $this->call(DataTypesTableSeeder::class);
+        // $this->call(MenuItemsTableSeeder::class);
+        // $this->call(RolesTableSeeder::class);
+        // $this->call(PermissionRoleTableSeeder::class);
+        // $this->call(PermissionsTableSeeder::class);
+        // $this->call(SettingsTableSeeder::class);
         $this->call(OkjaSeeder::class);
         $this->call(CategoriesTableSeeder::class);
         $this->call(BrandsTableSeeder::class);
