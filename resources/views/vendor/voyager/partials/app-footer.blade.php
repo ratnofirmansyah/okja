9a5c1c2acf078163452e018959ae4d0eb9649c32
ratnofirmansyah <asturof11@gmail.com>
2020-12-24 04:49:34 +0700

    edit footer

---
+++ resources/views/vendor/voyager/partials/app-footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="app-footer">
+    <div class="site-footer-right">
+        Made with <i class="voyager-heart"></i> by <a href="https://www.instagram.com/rfirman20/" target="_blank">Kizuked</a>
+    </div>
+</footer>
