04721666ba435bb76472cfd9745299367af291fb
ratnofirmansyah <asturof11@gmail.com>
2020-12-24 01:05:36 +0700

    comment setting seeder

--- database/seeders/OkjaSeeder.php
+++ database/seeders/OkjaSeeder.php
@@ -2473,119 +2473,119 @@ class OkjaSeeder extends Seeder
             ),
         ));
 
-		\DB::table('settings')->delete();
+		// \DB::table('settings')->delete();
         
-        \DB::table('settings')->insert(array (
-            0 => 
-            array (
-                'id' => 1,
-                'key' => 'site.title',
-                'display_name' => 'Site Title',
-                'value' => 'Okja',
-                'details' => '',
-                'type' => 'text',
-                'order' => 1,
-                'group' => 'Site',
-            ),
-            1 => 
-            array (
-                'id' => 2,
-                'key' => 'site.description',
-                'display_name' => 'Site Description',
-                'value' => 'Okja is a simple and free data-stock management for small or start up bussiness.',
-                'details' => '',
-                'type' => 'text',
-                'order' => 2,
-                'group' => 'Site',
-            ),
-            2 => 
-            array (
-                'id' => 3,
-                'key' => 'site.logo',
-                'display_name' => 'Site Logo',
-                'value' => 'settings\\October2020\\BSGoIuGHnd7hVsvyNUnw.jpg',
-                'details' => '',
-                'type' => 'image',
-                'order' => 3,
-                'group' => 'Site',
-            ),
-            3 => 
-            array (
-                'id' => 4,
-                'key' => 'site.google_analytics_tracking_id',
-                'display_name' => 'Google Analytics Tracking ID',
-                'value' => NULL,
-                'details' => '',
-                'type' => 'text',
-                'order' => 4,
-                'group' => 'Site',
-            ),
-            4 => 
-            array (
-                'id' => 5,
-                'key' => 'admin.bg_image',
-                'display_name' => 'Admin Background Image',
-                'value' => 'settings\\October2020\\4GoH0fZ8RzbROPrMWk10.jpg',
-                'details' => '',
-                'type' => 'image',
-                'order' => 5,
-                'group' => 'Admin',
-            ),
-            5 => 
-            array (
-                'id' => 6,
-                'key' => 'admin.title',
-                'display_name' => 'Admin Title',
-                'value' => 'Okja',
-                'details' => '',
-                'type' => 'text',
-                'order' => 1,
-                'group' => 'Admin',
-            ),
-            6 => 
-            array (
-                'id' => 7,
-                'key' => 'admin.description',
-                'display_name' => 'Admin Description',
-                'value' => 'Welcome to Okja. The Simple and easy to use data-stock management for your bussiness.',
-                'details' => '',
-                'type' => 'text',
-                'order' => 2,
-                'group' => 'Admin',
-            ),
-            7 => 
-            array (
-                'id' => 8,
-                'key' => 'admin.loader',
-                'display_name' => 'Admin Loader',
-                'value' => 'settings\\October2020\\495daELiT5eprhMYzxP6.jpg',
-                'details' => '',
-                'type' => 'image',
-                'order' => 3,
-                'group' => 'Admin',
-            ),
-            8 => 
-            array (
-                'id' => 9,
-                'key' => 'admin.icon_image',
-                'display_name' => 'Admin Icon Image',
-                'value' => 'settings\\October2020\\uXFtWNocVVbiLRyq3wvD.jpg',
-                'details' => '',
-                'type' => 'image',
-                'order' => 4,
-                'group' => 'Admin',
-            ),
-            9 => 
-            array (
-                'id' => 10,
-                'key' => 'admin.google_analytics_client_id',
-            'display_name' => 'Google Analytics Client ID (used for admin dashboard)',
-                'value' => NULL,
-                'details' => '',
-                'type' => 'text',
-                'order' => 1,
-                'group' => 'Admin',
-            ),
-        ));
+  //       \DB::table('settings')->insert(array (
+  //           0 => 
+  //           array (
+  //               'id' => 1,
+  //               'key' => 'site.title',
+  //               'display_name' => 'Site Title',
+  //               'value' => 'Okja',
+  //               'details' => '',
+  //               'type' => 'text',
+  //               'order' => 1,
+  //               'group' => 'Site',
+  //           ),
+  //           1 => 
+  //           array (
+  //               'id' => 2,
+  //               'key' => 'site.description',
+  //               'display_name' => 'Site Description',
+  //               'value' => 'Okja is a simple and free data-stock management for small or start up bussiness.',
+  //               'details' => '',
+  //               'type' => 'text',
+  //               'order' => 2,
+  //               'group' => 'Site',
+  //           ),
+  //           2 => 
+  //           array (
+  //               'id' => 3,
+  //               'key' => 'site.logo',
+  //               'display_name' => 'Site Logo',
+  //               'value' => 'settings\\October2020\\BSGoIuGHnd7hVsvyNUnw.jpg',
+  //               'details' => '',
+  //               'type' => 'image',
+  //               'order' => 3,
+  //               'group' => 'Site',
+  //           ),
+  //           3 => 
+  //           array (
+  //               'id' => 4,
+  //               'key' => 'site.google_analytics_tracking_id',
+  //               'display_name' => 'Google Analytics Tracking ID',
+  //               'value' => NULL,
+  //               'details' => '',
+  //               'type' => 'text',
+  //               'order' => 4,
+  //               'group' => 'Site',
+  //           ),
+  //           4 => 
+  //           array (
+  //               'id' => 5,
+  //               'key' => 'admin.bg_image',
+  //               'display_name' => 'Admin Background Image',
+  //               'value' => 'settings\\October2020\\4GoH0fZ8RzbROPrMWk10.jpg',
+  //               'details' => '',
+  //               'type' => 'image',
+  //               'order' => 5,
+  //               'group' => 'Admin',
+  //           ),
+  //           5 => 
+  //           array (
+  //               'id' => 6,
+  //               'key' => 'admin.title',
+  //               'display_name' => 'Admin Title',
+  //               'value' => 'Okja',
+  //               'details' => '',
+  //               'type' => 'text',
+  //               'order' => 1,
+  //               'group' => 'Admin',
+  //           ),
+  //           6 => 
+  //           array (
+  //               'id' => 7,
+  //               'key' => 'admin.description',
+  //               'display_name' => 'Admin Description',
+  //               'value' => 'Welcome to Okja. The Simple and easy to use data-stock management for your bussiness.',
+  //               'details' => '',
+  //               'type' => 'text',
+  //               'order' => 2,
+  //               'group' => 'Admin',
+  //           ),
+  //           7 => 
+  //           array (
+  //               'id' => 8,
+  //               'key' => 'admin.loader',
+  //               'display_name' => 'Admin Loader',
+  //               'value' => 'settings\\October2020\\495daELiT5eprhMYzxP6.jpg',
+  //               'details' => '',
+  //               'type' => 'image',
+  //               'order' => 3,
+  //               'group' => 'Admin',
+  //           ),
+  //           8 => 
+  //           array (
+  //               'id' => 9,
+  //               'key' => 'admin.icon_image',
+  //               'display_name' => 'Admin Icon Image',
+  //               'value' => 'settings\\October2020\\uXFtWNocVVbiLRyq3wvD.jpg',
+  //               'details' => '',
+  //               'type' => 'image',
+  //               'order' => 4,
+  //               'group' => 'Admin',
+  //           ),
+  //           9 => 
+  //           array (
+  //               'id' => 10,
+  //               'key' => 'admin.google_analytics_client_id',
+  //           'display_name' => 'Google Analytics Client ID (used for admin dashboard)',
+  //               'value' => NULL,
+  //               'details' => '',
+  //               'type' => 'text',
+  //               'order' => 1,
+  //               'group' => 'Admin',
+  //           ),
+  //       ));
     }
 }
