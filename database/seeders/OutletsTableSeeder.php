2aeb4f5d7ed3fabd657455ec8565d4d375c30e02
ratnofirmansyah <asturof11@gmail.com>
2020-12-25 02:14:13 +0700

    update seeder

--- database/seeders/OutletsTableSeeder.php
+++ database/seeders/OutletsTableSeeder.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Seeder;
 
 class OutletsTableSeeder extends Seeder
 {
+
+    /**
+     * Auto generated seed file
+     *
+     * @return void
+     */
     public function run()
     {
         
@@ -23,6 +29,31 @@ class OutletsTableSeeder extends Seeder
                 'longitude' => '110.416504',
                 'created_at' => '2020-10-04 05:10:49',
                 'updated_at' => '2020-10-04 05:10:49',
+                'deleted_at' => NULL,
+            ),
+            1 => 
+            array (
+                'id' => 2,
+                'brand_id' => 2,
+                'name' => 'Rangga Sembako Mlati',
+                'address' => 'Mlati Sleman Yogyakarta',
+                'latitude' => NULL,
+                'longitude' => NULL,
+                'created_at' => '2020-12-23 21:00:45',
+                'updated_at' => '2020-12-23 21:00:45',
+                'deleted_at' => NULL,
+            ),
+            2 => 
+            array (
+                'id' => 3,
+                'brand_id' => 1,
+                'name' => 'Firman Sembako Klipang',
+                'address' => 'Klipang Semarang Jawa Tengah',
+                'latitude' => NULL,
+                'longitude' => NULL,
+                'created_at' => '2020-12-24 13:25:30',
+                'updated_at' => '2020-12-24 13:25:30',
+                'deleted_at' => NULL,
             ),
         ));
         
