179c06fc3d74a8918b1a21e89ea20bacb8174b7a
ratnofirmansyah <asturof11@gmail.com>
2021-12-20 15:16:09 +0700

    add monthly report controller

--- app/Http/Controllers/Report.php
+++ app/Http/Controllers/Report.php
@@ -93,6 +93,87 @@ class Report extends Controller
         $data['transactions'] = $dataTransactions;
 
         return Voyager::view('vendor.voyager.report.daily_transaction', $data);
+    }
 
+    public function monthlyTransaction(Request $request)
+    {
+        $user = Auth::user();
+        if ($user->role_id == 3) {
+            $data['outlets'] = Outlet::where('brand_id', $user->brand_id)->get();
+        } elseif ($user->role_id == 2){
+            $data['outlets'] = Outlet::where('id', $user->outlet_id)->get();
+        }else{
+            $data['outlets'] = Outlet::all();
+        }
+
+        $daterange = explode(' - ', $request->get('daterange', date('Y-m', strtotime('-12 months')).' - '.date('Y-m')));
+        $data['daterangeori'] = date('Y-m', strtotime($daterange[0])).' - '.date('Y-m', strtotime($daterange[1]));
+
+        // --- Get by daily ---
+            $selectedColumns = [
+                DB::raw('date_format(user_transaction_histories.created_at, "%Y-%m") as date'),
+                DB::raw('sum(user_transaction_histories.qty) as total')
+            ];
+            $transactionDaily = DB::table('user_transaction_histories')
+                ->select($selectedColumns)
+                ->join('outlets', 'outlets.id', '=', 'user_transaction_histories.outlet_id')
+                ->whereBetween(DB::raw('date_format(user_transaction_histories.created_at, "%Y-%m")'), [$daterange[0], $daterange[1]])
+                ->whereNull('user_transaction_histories.deleted_at');
+
+            if ($user->role_id == 3 || $user->role_id == 2) {
+                $outletIds = $data['outlets']->pluck('id')->toArray();
+                $transactionDaily = $transactionDaily->whereIn('user_transaction_histories.outlet_id', $outletIds);
+            }
+
+            $transactionDaily = $transactionDaily->groupBy(DB::raw('date_format(user_transaction_histories.created_at, "%Y-%m")'))->get();
+
+        // --- Get by Outlet ---
+            $selectedColumns = [
+                'outlets.name',
+                'outlets.id'
+            ];
+            $transactionOutlets = DB::table('user_transaction_histories')
+                ->select($selectedColumns)
+                ->join('outlets', 'outlets.id', '=', 'user_transaction_histories.outlet_id')
+                ->whereBetween(DB::raw('date_format(user_transaction_histories.created_at, "%Y-%m")'), [$daterange[0], $daterange[1]])
+                ->whereNull('user_transaction_histories.deleted_at');
+
+            if ($user->role_id == 3 || $user->role_id == 2) {
+                $outletIds = $data['outlets']->pluck('id')->toArray();
+                $transactionOutlets = $transactionOutlets->whereIn('user_transaction_histories.outlet_id', $outletIds);
+            }
+
+            $transactionOutlets = $transactionOutlets->groupBy('outlets.name')->groupBy('outlets.id')->get();
+
+        $dataTransactions = array();
+        foreach ($transactionOutlets as $key => $outlet) {
+            $dataTransactions[$key] = (object) array();
+            $dataTransactions[$key]->label = $outlet->name;
+            $dataTransactions[$key]->data = array();
+            foreach ($transactionDaily as $index => $daily) {
+                // --- Get by Outlet daily ---
+                    $selectedColumns = [
+                        DB::raw('sum(user_transaction_histories.qty) as total')
+                    ];
+                    $transactionDailyByOutlet = DB::table('user_transaction_histories')
+                        ->select($selectedColumns)
+                        ->join('outlets', 'outlets.id', '=', 'user_transaction_histories.outlet_id')
+                        ->where('outlets.id', $outlet->id)
+                        ->where(DB::raw('date_format(user_transaction_histories.created_at, "%Y-%m")'), $daily->date)
+                        ->whereNull('user_transaction_histories.deleted_at');
+
+                    $transactionDailyByOutlet = $transactionDailyByOutlet->first();
+                if ($transactionDailyByOutlet) {
+                    array_push($dataTransactions[$key]->data, $transactionDailyByOutlet->total);
+                }else{
+                    array_push($dataTransactions[$key]->data, 0);
+                }
+            }
+        }
+
+        $data['labels'] = $transactionDaily->pluck('date')->toArray();
+        $data['transactions'] = $dataTransactions;
+
+        return Voyager::view('vendor.voyager.report.monthly_transaction', $data);
     }
 }
