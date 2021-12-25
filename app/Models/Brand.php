09a176b393a31f3cafb0d05bea50cbb608eb81f3
ratnofirmansyah <asturof11@gmail.com>
2021-12-25 16:06:40 +0700

    update model

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -21,4 +21,8 @@ class Brand extends Model
     ];
 
     protected $hidden = ['created_at', 'updated_at', 'deleted_at'];
+
+    public function category() {
+        return $this->hasMany('App\Models\Category');
+    }
 }
